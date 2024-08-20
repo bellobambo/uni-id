4b67f4f92f26544eb446a726540966945846a281
bellobambo <bellobambo21@gmail.com>
2024-08-20 13:57:25 +0100

    payload

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -18,7 +18,7 @@ const page = () => {
   };
 
   return (
-    <div className="min-h-screen flex mt-[150px] items-center  flex-col">
+    <div className="min-h-screen flex mt-[100px] items-center  flex-col">
       <motion.i
         className="block text-center text-lg text-[#FAD572] mb-6"
         initial={{ opacity: 0, y: -20 }}
@@ -39,33 +39,100 @@ const page = () => {
           UID
         </div>
         <motion.form
-          className="space-y-4"
+          className="space-y-4 items-center "
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, delay: 0.4 }}
         >
-          <motion.div animate={controls}>
-            <label className="block text-[#061867] font-semibold mb-2">
-              Matric Number
-            </label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border border-[#061867] rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent bg-gray-300 text-black"
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-          </motion.div>
-          <motion.div animate={controls}>
-            <label className="block text-[#061867] font-semibold mb-2">
-              Hostel
-            </label>
-            <input
-              type="text"
-              className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black"
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-          </motion.div>
+          <div className="flex items-center  gap-6 w-fit">
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Matric Number
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent bg-gray-300 text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Hostel
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+          </div>
+
+          <div className="flex items-center  gap-6">
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Passport
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Signature
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+          </div>
+
+          <div className="flex items-center  gap-6">
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Certificate
+              </label>
+              <input
+                placeholder="O'level , A'level or OND"
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+          </div>
+
+          <div>
+            <motion.div animate={controls}>
+              <label className="block text-[#061867] font-semibold mb-2">
+                NIN
+              </label>
+              <input
+                type="text"
+                className="w-full px-4 py-2 border border-[#061867] bg-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#061867] focus:border-transparent text-black "
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+              />
+            </motion.div>
+          </div>
         </motion.form>
       </div>
     </div>
